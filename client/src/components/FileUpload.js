66408c4fb035054c4343a34bd09614f23b7d3d28
eric <ericblancas23@gmail.com>
2019-06-03 00:40:13 -0700

    changed alt to empty string

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -49,7 +49,7 @@ const FileUpload = () => {
             {uploadedFile ? <div className="row mt-5">
                 <div className="col-md-6 m-auto">
                     <h3 className="text-center">{uploadedFile.fieldName}</h3>
-                    <img style={{ width: '100%'}} src={uploadedFile.filePath} alt="uploaded file"></img>
+                    <img style={{ width: '100%'}} src={uploadedFile.filePath} alt=""></img>
                 </div>
             </div> : null}
         </Fragment>
